864a4719774053cbecee6f4eaadfbabeada1b8ae
radoslav <radoslav@dodek.sk>
2018-10-08 09:34:30 +0200

    In progress: Build and deploy scripts

--- refresh_publish/build.py
+++ refresh_publish/build.py
@@ -26,13 +26,27 @@ def build_dist_package():
     Build distribution package
     '''
 
+    # Update GIT repo
+    # TODO
+
     log('--> Install node modules')
     subprocess.call(["npm", "install"], cwd='../')
+
+    log('--> Update sites')
+    subprocess.call(["node", "generate-sites.js"], cwd='../')
 
     log('--> Create distribution binary file')
     subprocess.call(["grunt"], cwd='../')
 
+    # Update extension in Webstore
+    # TODO
 
+    # Push changes to GIT repo
+    log('--> Push changes to GIT repo')
+    subprocess.call(["git", "add", "extension/sites.js"], cwd='../')
+    subprocess.call(["git", "commit", "-m", "Version updated"], cwd='../')
+    subprocess.call(["git", "push"], cwd='../')
+    # TODO
 
 
 if __name__ == '__main__':
