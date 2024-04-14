a49701a42c3b1020de164b4c9c5ddb38f8a75409
Rado Dodek <radoslav@dodek.sk>
2024-04-14 17:36:38 +0200

    Update sites

--- generate-sites.js
+++ generate-sites.js
@@ -3,7 +3,7 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 
-const domain_list_url = 'https://www.konspiratori.sk/assets/downloads/zoznam.txt';
+const domain_list_url = 'https://konspiratori.sk/static/lists/zoznam.txt';
 const sites_file = "extension/sites.js";
 
 // Fetch the list of domains and create 'extension/sites.js' file
